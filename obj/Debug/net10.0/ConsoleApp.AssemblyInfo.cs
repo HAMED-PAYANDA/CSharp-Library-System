766f9c7ebadad4b417187981b4eb0080bce0a337
HAMED-PAYANDA <hamedpayanda@gmail.com>
2026-08-26 11:20:12 -0400

    Add .gitignore and remove compiled binaries

--- obj/Debug/net10.0/ConsoleApp.AssemblyInfo.cs
+++ obj/Debug/net10.0/ConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d67a2cb5ba2d98516c2f7d93a1ef9b9f605b2c08")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
